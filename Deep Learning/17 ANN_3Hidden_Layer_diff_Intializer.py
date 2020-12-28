c3cc500711ab4757b36d784a6ff8e065d700f7eb
sabeer-ph <shabz.sjec@gmail.com>
2020-12-28 19:05:44 +0400

    17 ANN_3Hidden_Layer_diff_Intializer

---
+++ Deep Learning/17 ANN_3Hidden_Layer_diff_Intializer.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 19:47:03 2020
+
+@author: Sabeer PH
+
+# here will be use 3 hidden layers with different amount of neurons
+# also we will play around changing intializers changed the intializers to "he_normal" from "he_uniform"
+"""
+# Artificial Neural Network
+
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('./Data/17-Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13]
+y = dataset.iloc[:, 13]
+
+#Create dummy variables
+geography=pd.get_dummies(X["Geography"],drop_first=True)
+gender=pd.get_dummies(X['Gender'],drop_first=True)
+
+## Concatenate the Data Frames
+
+X=pd.concat([X,geography,gender],axis=1)
+
+#dataset['Geography'].unique()
+
+## Drop Unnecessary columns
+X=X.drop(['Geography','Gender'],axis=1)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LeakyReLU,PReLU,ELU
+from keras.layers import Dropout
+
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 10, kernel_initializer= 'he_normal',activation='relu',input_dim = 11))
+# input_din is no of colums from input - check x_train
+# units means no of neurons in first layer
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 20, kernel_initializer = 'he_normal',activation='relu'))
+
+# Adding the third hidden layer
+classifier.add(Dense(units = 15, kernel_initializer = 'he_normal',activation='relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'glorot_uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'Adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# loss is binary_crossentropy coz its clasification of 2 outputs
+
+
+# summary of classifier
+classifier.summary()
+
+# Fitting the ANN to the Training set
+model_history=classifier.fit(X_train, y_train,validation_split=0.33, batch_size = 10, epochs = 100)
+
+# list all data in history
+
+print(model_history.history.keys())
+# summarize history for accuracy
+plt.plot(model_history.history['acc'])
+plt.plot(model_history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Calculate the Accuracy
+from sklearn.metrics import accuracy_score
+score=accuracy_score(y_pred,y_test)
+
+
+# summary : accuracy increased by 1 compared to previous
+
